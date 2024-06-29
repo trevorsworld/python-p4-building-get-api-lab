fc13e77598b0783d803cbb9902ccafa8dc290436
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-06-29 17:12:48 +0300

    Completed Code Along

--- server/app.py
+++ server/app.py
@@ -20,19 +20,45 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    
+    response = make_response(
+        bakeries,
+        200
+    )
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = bakery.to_dict()
+    
+    response = make_response(
+        bakery_dict,
+        200
+    )
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = [baked_goods.to_dict() for baked_good in BakedGood.query.order_by(BakedGood.price.desc().all())]
+    
+    response = make_response(
+        baked_goods,
+        200
+    )
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    most_expensive_baked_good_dict = most_expensive.to_dict()
+    
+    response = make_response(
+        most_expensive_baked_good_dict,
+        200
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
